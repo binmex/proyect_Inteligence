839ae75ca494f850d30d581db5ad212edff0493f
binmex <robin019@hotmail.com>
2024-02-14 11:47:03 -0500

    PLN por corpus

--- controllers/controller_nlp.js
+++ controllers/controller_nlp.js
@@ -1,30 +1,63 @@
-const { NlpManager } = require('node-nlp');
+const { NormalizerEs, TokenizerEs, StopwordsEs } = require("@nlpjs/lang-es");
+const { Container } = require("@nlpjs/core");
+const { SentimentAnalyzer } = require("@nlpjs/sentiment");
+const { LangEs } = require("@nlpjs/lang-es");
+const fs = require("fs");
 
-const manager = new NlpManager({ languages: ['es'] });
+const normalizer = new NormalizerEs();
+const token = new TokenizerEs();
+const stopwords = new StopwordsEs();
 
+const corpus = JSON.parse(fs.readFileSync("helpers/entityPln.json", "utf-8"));
 
-manager.addNamedEntityText('tamaño', 'grande', ['es'], 'tamaño');
-manager.addNamedEntityText('tamaño', 'pequeña', ['es'], 'tamaño');
-manager.addNamedEntityText('objeto', 'silla', ['es'], 'objeto');
-manager.addNamedEntityText('objeto', 'mesa', ['es'], 'objeto');
-manager.addNamedEntityText('color', 'rojo', ['es'], 'color');
-manager.addNamedEntityText('color', 'azul', ['es'], 'color');
-manager.addBetweenCondition('tamaño', 'entre', 'y');
-manager.addAfterCondition('color', 'de');
-
-
-(async () => {
-    await manager.train();
-    manager.save();
-})();
-
+async function analyzeSentiment(texto) {
+  const container = new Container();
+  container.use(LangEs);
+  const sentiment = new SentimentAnalyzer({ container });
+  const result = await sentiment.process({
+    locale: "es",
+    text: texto,
+  });
+  return result.sentiment
+}
 
 exports.processText = async (req, res) => {
-    const { text } = req.body;
-    try {
-        const response = await manager.process('es', text);
-        res.json(response);
-    } catch (error) {
-        res.status(500).json({ error: 'Error en el procesamiento NLP' });
+
+  const { text } = req.body;
+  let found = 0;
+  let noFound = 0;
+  let wordNoFound = [];
+  let wordFound = [];
+  try {
+    const result = normalizer.normalize(text); //retorna vector con palabras a buscar
+
+
+    //const result2 = await analyzeSentiment(text);
+    //console.log(result2);
+
+
+    const final = stopwords.removeStopwords(token.tokenize(result, true)); //elimina palabras vacias con,el,a y tokeniza
+
+    final.forEach((evaluar) => {
+      let variable = corpus.values.find((element) => evaluar == element);
+      if (variable != undefined) {
+        found += 1;
+        wordFound.push(variable);
+      } else {
+        noFound += 1;
+        wordNoFound.push(evaluar);
+      }
+    });
+
+    const holgura = (found * 100) / final.length;
+
+    //validamos que respuesta devolvemos
+    if (holgura < 100) {
+      res.status(404).json({ state: false, data: wordNoFound });
+    } else {
+      res.status(200).json({ state: true, data: wordFound });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
