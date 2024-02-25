cf07f1731487ee42c32889b90be339dfa2021381
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-02-25 13:40:15 -0500

    update method generate color

--- controllers/controller_colormind.js
+++ controllers/controller_colormind.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 exports.generateColor = async (req, res) => {
   var url = "http://colormind.io/api/";
   const { color } = req.body;
@@ -6,23 +8,33 @@ exports.generateColor = async (req, res) => {
     input: [color, "N", "N", "N", "N"],
   };
 
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      var palette = data.result;
-
-      res.status(200).json({ state: true, data: JSON.stringify(palette) });
-    })
-    .catch((err) => {
-      res.status(500).json({ state: false, error: err.message });
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     });
+
+    const responseData = await response.json();
+    const palette = responseData.result;
+
+    
+    const colorsObject = {
+      color1: `rgb(${palette[0].join(',')})`,
+      color2: `rgb(${palette[1].join(',')})`,
+      color3: `rgb(${palette[2].join(',')})`,
+      color4: `rgb(${palette[3].join(',')})`,
+      color5: `rgb(${palette[4].join(',')})`,
+    };
+
+    res.status(200).json({ state: true, data: colorsObject });
+  } catch (err) {
+    res.status(500).json({ state: false, error: err.message });
+  }
 };
+
 
 /**Generar lista de modos disponibles */
 exports.listModels = async (req, res) => {
--- routes/colormind.js
+++ routes/colormind.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 const {generateColor,listModels} = require("../controllers/controller_colormind")
 
-router.get('/',generateColor);
+router.post('/',generateColor);
 router.get('/listModels',listModels)
 
 module.exports = router;
